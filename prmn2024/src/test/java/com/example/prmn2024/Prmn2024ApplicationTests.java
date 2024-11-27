135d0c012875588cf976c45bd3d33f717727717c
okamura-keisuke <b2220530@photon.chitose.ac.jp>
2024-11-27 16:06:42 +0900

    first

---
+++ prmn2024/src/test/java/com/example/prmn2024/Prmn2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prmn2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prmn2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
